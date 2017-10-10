998b22ab5e816e702e4cdc595fab8b4972e5cffd
Olya\Olga <olya.kurzeneva@mail.ru>
2017-10-10 14:42:53 +0400

    Вывод данных при нажатиии на кнопку

--- lab1/src/main/java/org/rip/olga/Car.java
+++ lab1/src/main/java/org/rip/olga/Car.java
@@ -1,6 +1,7 @@
 package org.rip.olga;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,22 +12,34 @@ public class Car extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         response.setContentType("text/html; charset=windows-1251");
+        PrintWriter out = response.getWriter();
 
         String model = (String) request.getParameter("model");
         String color = (String) request.getParameter("color");
-        response.getWriter().println("<!DOCTYPE HTML>");
-        response.getWriter().println("<form action=\"\" method=\"GET\">");
-        response.getWriter().println("<p>Введите модель машины: <input type=\"text\" name=\"model\"></p>");
-        response.getWriter().println("<p>Введите цвет машины: <input type=\"text\" name=\"color\"></p>");
-        response.getWriter().println("<input type=\"submit\" value=\"Отправить\" />");
-        response.getWriter().println("</form>");
-        response.getWriter().println("<html><body><p>" + model + "</p></body></html>");
-        response.getWriter().println("<html><body><p>" + color + "</p></body></html>");
+        out.println("<!DOCTYPE HTML>");
+        out.println("<form action=\"\" method=\"GET\">");
+        out.println("<p>Введите модель машины: <input type=\"text\" name=\"model\"></p>");
+        out.println("<p>Введите цвет машины: <input type=\"text\" name=\"color\"></p>");
+        out.println("<input type=\"submit\" value=\"Добавить\" />");
+        out.println("</form>");
+
+        response.getWriter().println("<form action=\"\" method=\"POST\">");
+        out.println("<input type=\"submit\" value=\"Список внесенных сущностей\" />");
+        out.println("</form>");
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException
     {
         doGet(request, response);
+
+        String model = (String) request.getParameter("model");
+        String color = (String) request.getParameter("color");
+
+        response.getWriter().println("<!DOCTYPE HTML>");
+        response.getWriter().println("<form action=\"\" method=\"POST\">");
+        response.getWriter().println("<html><body><p>Модель:" + model + "</p></body></html>");
+        response.getWriter().println("<html><body><p>Цвет:" + color + "</p></body></html>");
+        response.getWriter().println("</form>");
     }
 }
