fb74e27078d56eb0ff4056d38e1573a5f39406c8
Olya\Olga <olya.kurzeneva@mail.ru>
2017-10-11 16:10:43 +0400

    Правильно работающий массив

--- lab1/src/main/java/org/rip/olga/Car.java
+++ lab1/src/main/java/org/rip/olga/Car.java
@@ -2,20 +2,22 @@ package org.rip.olga;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Car extends HttpServlet {
+    private ArrayList cars = new ArrayList();
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
         response.setContentType("text/html; charset=windows-1251");
         PrintWriter out = response.getWriter();
 
-        String model = (String) request.getParameter("model");
-        String color = (String) request.getParameter("color");
         out.println("<!DOCTYPE HTML>");
         out.println("<form action=\"\" method=\"GET\">");
         out.println("<p>Введите модель машины: <input type=\"text\" name=\"model\"></p>");
@@ -26,20 +28,33 @@ public class Car extends HttpServlet {
         response.getWriter().println("<form action=\"\" method=\"POST\">");
         out.println("<input type=\"submit\" value=\"Список внесенных сущностей\" />");
         out.println("</form>");
+
+        request.setAttribute("cars", cars);
     }
 
+    @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response )
             throws ServletException, IOException
     {
         doGet(request, response);
+        PrintWriter out = response.getWriter();
 
         String model = (String) request.getParameter("model");
         String color = (String) request.getParameter("color");
+        
+        cars.add(model);
+        cars.add(color);
 
-        response.getWriter().println("<!DOCTYPE HTML>");
-        response.getWriter().println("<form action=\"\" method=\"POST\">");
-        response.getWriter().println("<html><body><p>Модель:" + model + "</p></body></html>");
-        response.getWriter().println("<html><body><p>Цвет:" + color + "</p></body></html>");
-        response.getWriter().println("</form>");
+        String car = "";
+        for (int i = 0; i < cars.size(); i++) {
+            car = car + cars.get(i) + " ";
+        }
+        out.println(car);
+
+        out.println("<!DOCTYPE HTML>");
+        out.println("<form action=\"\" method=\"POST\">");
+        out.println("<html><body><p>Модель:" + model + "</p></body></html>");
+        out.println("<html><body><p>Цвет:" + color + "</p></body></html>");
+        out.println("</form>");
     }
 }
